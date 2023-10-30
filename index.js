57aa69d9594aaf8c683dc6e57ec35b9e98e4c77e
Samuel Cerón <wstceron@gmail.com>
2023-10-30 16:10:13 -0500

    1.1.0

--- index.js
+++ index.js
@@ -15,13 +15,49 @@ function App() {
 
 function Router() {
 	this._routes = {};
-	this._handle = function (req, res) {
-		const { method, url } = req;
-        console.log(url);
+	this._handle = async function (req, res) {
+		const { method, body, queryParams, pathName } = await this.parseRequest(req);
 		this._routes[url][method.toLowerCase()](req, res);
 	};
+	this.parseRequest = async function (req) {
+		function parseBody(){
+			return new Promise((resolve, reject) => {
+				let body = "";
+				req.on("data", (chunk) => {
+					body += chunk;
+				});
+				req.on("error", (chunk) => {
+					reject("Error")
+				});
+				req.on("end", () => {
+					body = JSON.parse(body);
+					resolve(body)
+				});
+			})
+		}
+		function parseUrl(url){
+			const {pathName,searchParams} = new URL(url);
+			const queryParams = {}
+			searchParams.forEach((value,key)=>{
+				queryParams[key] = value;
+			})
+			return {
+				pathName,
+				queryParams
+			}
+		}
+		const { method, url:oldUrl } = req;
+		const path = `http://${req.headers.host}${oldUrl}`
+		const {queryParams, pathName} = parseUrl(path)
+		const body = await parseBody();
+		return {
+			method,
+			body,
+			queryParams,
+			pathName
+		};
+	};
 }
-
 
 function createRouter() {
 	http.METHODS.forEach((_method) => {
